a54cf69a455d5dc2554cec1670384ef1b8635df2
SegurancaDPDF <136491999+SegurancaDPDF@users.noreply.github.com>
2023-06-23 13:30:10 -0300

    Delete thothsigner_client directory

--- thothsigner_client/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThothSignerClientAppConfig(AppConfig):
-    name = 'thothsigner_client'
-    verbose_name = "ThothSigner Client"
--- thothsigner_client/exceptions.py
+++
@@ -1,9 +0,0 @@
-from core.exceptions import CoreBaseException
-
-
-class ThothsignerFailed(CoreBaseException):
-    message = 'Falha ao assinar documento com assinador Thoth Signer.'
-
-
-class ThothsignerUnavailable(CoreBaseException):
-    message = 'O Assinador de documentos Thoth Signer está indisponível no momento.'
