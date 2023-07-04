f207b91470c67157df7206cd91ee8942fc4de88a
SegurancaDPDF <136491999+SegurancaDPDF@users.noreply.github.com>
2023-07-04 12:47:38 -0300

    Delete core directory

--- core/exceptions.py
+++
@@ -1,7 +0,0 @@
-class CoreBaseException(Exception):
-    message = ''
-
-    # TODO: Verificar necessidade da sobrescrita do Exception
-    def __init__(self, msg=None, *args, **kwargs):
-        msg = msg or self.message
-        super().__init__(msg, *args, **kwargs)
--- core/pg_database_expressions.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Func
-
-
-class FuncDateUTC(Func):
-    function = 'DATE'
-    template = "%(function)s(%(expressions)s AT TIME ZONE 'UTC')"
