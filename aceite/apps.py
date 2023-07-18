0516667a23654767c9d8acec956b6509bfdea371
SegurancaDPDF <136491999+SegurancaDPDF@users.noreply.github.com>
2023-07-18 10:40:28 -0300

    Delete aceite directory

--- aceite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AceiteAppConfig(AppConfig):
-    name = 'aceite'
-    verbose_name = "Aceite"
--- aceite/routers.py
+++
@@ -1,9 +0,0 @@
-# third-party
-from rest_framework import routers
-
-# application
-from . import viewsets
-
-router = routers.SimpleRouter()
-router.register(r'termos', viewsets.TermoViewSet)
-router.register(r'termos-respostas', viewsets.TermoRespostaViewSet)
