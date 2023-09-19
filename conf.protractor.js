3103e4220c1392e17bdd731d20885d52af6f3074
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-09-19 13:29:02 -0300

    Delete conf.protractor.js

--- conf.protractor.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-    framework: 'jasmine',
-    specs: ['**/*spec.js'],
-    allScriptsTimeout: 50000,
-    jasmineNodeOpts: { defaultTimeoutInterval: 260000 },
-    capabilities: {
-        'browserName': 'chrome'
-    },
-}
